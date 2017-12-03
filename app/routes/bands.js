1e8978c416b80e765aec1f04220883acf2cc2297
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-02 21:50:13 -0600

    Adding models for bands in band route

--- app/routes/bands.js
+++ app/routes/bands.js
@@ -1,4 +1,18 @@
 import Route from '@ember/routing/route';
+import EmberObject from '@ember/object';
+
+var Band = EmberObject.extend({
+  name:"",
+});
 
 export default Route.extend({
+
+  model: function(){
+    var ledZepellin1 = Band.create({ name: 'Led Zepellin1'});
+    var ledZepellin2 = Band.create({ name: 'Led Zepellin2'});
+    var ledZepellin3 = Band.create({ name: 'Led Zepellin3'});
+    return [ledZepellin1, ledZepellin2, ledZepellin3]
+  }
+
+
 });
