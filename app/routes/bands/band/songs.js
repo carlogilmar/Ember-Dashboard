34cea0ee3e44857567331883927ad34b7d78b8b7
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-03 02:57:17 -0600

    Agregando route bands/band/song.js

---
+++ app/routes/bands/band/songs.js
@@ -0,0 +1,21 @@
+import Song from 'rarwe/models/song';
+import Route from '@ember/routing/route';
+
+export default Route.extend({
+  model: function(){
+    return this.modelFor('bands.band');
+  },
+
+  actions: {
+    createSong: function(){
+      var controller = this.get('controller');
+      var band = this.modelFor('bands.band');
+      var title = controller.get('title');
+
+      var song = Song.create({ title:title, band: band});
+      band.get('songs').pushObject(song);
+      controller.set('title', "");
+      alert("Cancion agregada");
+    }
+  }
+});
