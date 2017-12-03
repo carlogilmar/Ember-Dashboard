eef46a160937d1acd1c1b8512f37c087a73a34ed
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-03 14:18:21 -0600

    #6 Agregando atributo descripcionm

--- app/models/band.js
+++ app/models/band.js
@@ -4,6 +4,7 @@ import EmberObject, {computed} from '@ember/object';
 //export default DS.Model.extend({
 export default EmberObject.extend({
   name:"",
+  description:"",
 
   init: function(){
     this._super(...arguments);
