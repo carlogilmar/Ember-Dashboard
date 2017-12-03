ec08c03c8fd0338d1b25dd687b6deafb4e7d416f
Carlo Gilmar <carlogilmar12@gmail.com>
2017-12-03 13:30:15 -0600

    Agregando controller para songs

---
+++ app/controllers/bands/band/songs.js
@@ -0,0 +1,10 @@
+import Controller from '@ember/controller';
+
+export default Controller.extend({
+  actions: {
+    updateRating: function(params){
+      var song = params.item, rating = params.rating;
+      song.set('rating', rating);
+    }
+  }
+});
